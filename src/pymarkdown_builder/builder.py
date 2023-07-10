e84bdfa536a192bade8caea519a7386ac1517d51
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-07-10 19:23:36 -0400

    refactor: remove write mode state management and use only available methods

--- src/pymarkdown_builder/builder.py
+++ src/pymarkdown_builder/builder.py
@@ -1,51 +1,13 @@
 """A Markdown document builder with line and span writing modes."""
 
 from dataclasses import dataclass, field
-from enum import Enum
-from functools import wraps
-from typing import Callable
 
-from typing_extensions import Concatenate, ParamSpec, Self, TypeVar
+from typing_extensions import ParamSpec, Self, TypeVar
 
 
 TMarkdownBuilder = TypeVar("TMarkdownBuilder", bound="MarkdownBuilder")
 Params = ParamSpec("Params")
 TReturn = TypeVar("TReturn")
-
-
-class WriteMode(Enum):
-    """The writing mode of the builder."""
-
-    LINE = "line"
-    """Line mode."""
-
-    SPAN = "span"
-    """Span mode."""
-
-
-def fixed_write_mode(mode: WriteMode):
-    """A decorator to set the writing mode of a builder method."""
-
-    def decorator(
-        method: Callable[Concatenate[TMarkdownBuilder, Params], TReturn],
-    ) -> Callable[Concatenate[TMarkdownBuilder, Params], TReturn]:
-        @wraps(method)
-        def inner(
-            self: TMarkdownBuilder,
-            *args: Params.args,
-            **kwargs: Params.kwargs,
-        ) -> TReturn:
-            original_write_mode = self.mode
-
-            self.mode = mode
-            result = method(self, *args, **kwargs)
-            self.mode = original_write_mode
-
-            return result
-
-        return inner
-
-    return decorator
 
 
 @dataclass
@@ -55,72 +17,6 @@ class MarkdownBuilder:
     document: str = field(default="")
     """Content of the builder."""
 
-    mode: WriteMode = field(default=WriteMode.LINE, init=False)
-    """Write mode of the builder. See [`WriteMode`][pymarkdown_builder.builder.WriteMode]."""  # noqa: E501
-
-    def line_mode(self) -> Self:
-        """Sets the builder to `line` mode.
-
-        Returns:
-            The builder instance.
-        """
-        self.mode = WriteMode.LINE
-        return self
-
-    def span_mode(self) -> Self:
-        """Sets the builder to `span` mode.
-
-        Returns:
-            The builder instance.
-        """
-        self.mode = WriteMode.SPAN
-        return self
-
-    def toggle_mode(self) -> Self:
-        """Toggles between `line` and `span` modes.
-
-        Returns:
-            The builder instance.
-        """
-        if self.mode == WriteMode.LINE:
-            self.span_mode()
-
-        elif self.mode == WriteMode.SPAN:
-            self.line_mode()
-
-        return self
-
-    def is_mode(self, mode: WriteMode) -> bool:
-        """Checks if the builder is in the given mode.
-
-        Args:
-            mode (WriteMode): Mode to check.
-
-        Returns:
-            True if the builder is in the given mode, False otherwise.
-        """
-        return self.mode == mode
-
-    def write(self, /, text: str) -> Self:
-        """Appends the text to the current document.
-
-        If on `line`, mode,
-            the text will be prepended with double line breaks.
-
-        Args:
-            text (str): Text to be appended.
-
-        Returns:
-            The builder instance.
-        """  # noqa: E501
-        if self.is_mode(WriteMode.LINE) and self.document != "":
-            text = "\n\n" + text
-
-        self.document += text
-
-        return self
-
-    @fixed_write_mode(WriteMode.LINE)
     def write_lines(self, *lines: str) -> Self:
         """Joins the lines with double line breaks and appends to the document.
 
@@ -131,11 +27,14 @@ class MarkdownBuilder:
             The builder instance.
         """
         joined_lines = "\n\n".join(lines)
-        self.write(joined_lines)
+
+        if self.document != "":
+            self.document += "\n\n"
+
+        self.document += joined_lines
 
         return self
 
-    @fixed_write_mode(WriteMode.SPAN)
     def write_spans(self, *spans: str) -> Self:
         """Joins the spans and appends to the document.
 
@@ -146,7 +45,18 @@ class MarkdownBuilder:
             The builder instance.
         """
         joined_spans = "".join(spans)
-        self.write(joined_spans)
+
+        self.document += joined_spans
+
+        return self
+
+    def line_break(self) -> Self:
+        """Appends a line break to the document.
+
+        Returns:
+            The builder instance.
+        """
+        self.document += "\n\n"
 
         return self
 
@@ -156,3 +66,4 @@ class MarkdownBuilder:
 
     lines = write_lines
     spans = write_spans
+    br = line_break
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,101 +1,31 @@
-from pymarkdown_builder.builder import MarkdownBuilder, WriteMode
+from pymarkdown_builder.builder import MarkdownBuilder
 
 
-def test_line_mode_should_set_to_line_mode():
+def test_write_lines_should_not_prepend_lines_with_double_line_breaks_if_document_is_empty():
     builder = MarkdownBuilder()
-    builder.line_mode()
+    builder.write_lines(
+        "content",
+        "other content",
+    )
 
-    assert builder.mode == WriteMode.LINE
+    assert builder.document == "content\n\nother content"
 
 
-def test_span_mode_should_set_to_span_mode():
+def test_write_lines_should_prepend_lines_with_double_line_breaks_if_document_is_not_empty():
+    builder = MarkdownBuilder("# Title")
+    builder.write_lines(
+        "content",
+        "other content",
+    )
+
+    assert builder.document == "# Title\n\ncontent\n\nother content"
+
+
+def test_write_lines_should_return_builder_instance():
     builder = MarkdownBuilder()
-    builder.span_mode()
+    result = builder.write_lines("")
 
-    assert builder.mode == WriteMode.SPAN
-
-
-def test_line_mode_should_return_builder():
-    builder = MarkdownBuilder()
-
-    assert builder.line_mode() == builder
-
-
-def test_span_mode_should_return_builder():
-    builder = MarkdownBuilder()
-
-    assert builder.span_mode() == builder
-
-
-def test_toggle_mode_should_toggle_mode():
-    builder = MarkdownBuilder()
-
-    assert builder.mode == WriteMode.LINE
-    assert builder.toggle_mode().mode == WriteMode.SPAN
-    assert builder.toggle_mode().mode == WriteMode.LINE
-
-
-def test_toggle_mode_should_return_builder():
-    builder = MarkdownBuilder()
-
-    assert builder.toggle_mode() == builder
-
-
-def test_is_mode_should_return_true_if_mode_is_correct():
-    builder = MarkdownBuilder()
-
-    assert builder.is_mode(WriteMode.LINE) is True
-
-    builder.span_mode()
-
-    assert builder.is_mode(WriteMode.SPAN) is True
-
-
-def test_write_should_append_to_document_if_document_is_empty():
-    builder = MarkdownBuilder()
-    builder.write("Hello World!")
-
-    assert builder.document == "Hello World!"
-
-
-def test_write_should_prepend_text_with_double_line_breaks_if_document_is_not_empty():
-    builder = MarkdownBuilder("Hello World!")
-    builder.write("Hello World!")
-
-    assert builder.document == "Hello World!\n\nHello World!"
-
-    builder = MarkdownBuilder()
-    builder.write("Hello World!")
-    builder.write("Hello World!")
-
-    assert builder.document == "Hello World!\n\nHello World!"
-
-
-def test_write_lines_should_join_lines_with_double_line_breaks():
-    builder = MarkdownBuilder()
-    builder.write_lines("Hello World!", "Hello World!")
-
-    assert builder.document == "Hello World!\n\nHello World!"
-
-
-def test_write_lines_should_ignore_current_mode():
-    builder = MarkdownBuilder()
-    builder.span_mode()
-
-    builder.write_lines("Hello World!", "Hello World!")
-
-    assert builder.document == "Hello World!\n\nHello World!"
-
-
-def test_write_lines_should_not_change_mode():
-    builder = MarkdownBuilder()
-    builder.span_mode()
-
-    assert builder.mode == WriteMode.SPAN
-
-    builder.write_lines("Hello World!", "Hello World!")
-
-    assert builder.mode == WriteMode.SPAN
+    assert builder is result
 
 
 def test_write_spans_should_join_spans():
@@ -105,27 +35,22 @@ def test_write_spans_should_join_spans():
     assert builder.document == "Hello World!"
 
 
-def test_write_spans_should_ignore_current_mode():
+def test_write_spans_should_return_builder_instance():
     builder = MarkdownBuilder()
-    builder.line_mode()
+    result = builder.write_spans("")
 
-    builder.write_spans("Hello ", "World!")
-
-    assert builder.document == "Hello World!"
+    assert builder is result
 
 
-def test_write_spans_should_not_change_mode():
+def test_line_break_should_append_double_line_break():
     builder = MarkdownBuilder()
-    builder.line_mode()
 
-    assert builder.mode == WriteMode.LINE
+    builder.br()
 
-    builder.write_spans("Hello ", "World!")
-
-    assert builder.mode == WriteMode.LINE
+    assert builder.document == "\n\n"
 
 
 def test_str_should_return_document():
     builder = MarkdownBuilder("Hello World!")
 
-    assert str(builder) == "Hello World!"
+    assert str(builder) == builder.document == "Hello World!"
